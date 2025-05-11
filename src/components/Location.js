2faa72e8c7019bd2b247e59c28df3e8561b9bd5b
Priyank Gupta <priyaaank@gmail.com>
2025-05-11 14:12:53 +0530

    Updated contact section

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,11 +3,21 @@ import React from "react";
 export default function Location() {
   return (
     <div className="location">
-      <h2 className="location-title">LOCATION</h2>
-      <div className="location-detail">145 New street name</div>
-      <div className="location-detail">4530 City</div>
-      <div className="location-detail">Country</div>
-      <div className="location-detail">hello@radinasfoodcorner.com</div>
+      <h2 className="location-title">CONTACT US</h2>
+      <div className="location-detail">
+        <a href="https://maps.app.goo.gl/EFLhhQF4Hf6q1hiY6" target="_blank" rel="noreferrer">
+          HSR Layout, Bangalore
+        </a>
+      </div>
+      <div className="location-detail">
+        <a href="https://wa.me/919545966155">Radina - 9545966155</a>
+      </div>
+      <div className="location-detail">
+      <a href="https://maps.app.goo.gl/EFLhhQF4Hf6q1hiY6" target="_blank" rel="noreferrer">HSR Layout, Bangalore</a>
+      </div>
+      <div className="location-detail">
+        <a href="mailto:hello@radinasfoodcorner.com">hello@radinasfoodcorner.com</a>
+      </div>
     </div>
   );
 }
