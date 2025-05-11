a8014daa3db503626a620eb937f41c8edb04cae7
Priyank Gupta <priyaaank@gmail.com>
2025-05-11 12:53:18 +0530

    Removed Home href as we don't need it at the moment

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,6 @@ export default function Header() {
     <header className="header">
       <h1>Radina's Foodcorner</h1>
       <nav className="nav">
-        <a href="#">Home</a>
         <button className="order-btn">Contact</button>
       </nav>
     </header>
