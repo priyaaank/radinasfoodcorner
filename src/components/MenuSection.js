b74e72886fb224091a50a8769a4bdbaf69a3e166
Priyank Gupta <priyaaank@gmail.com>
2025-05-11 15:20:20 +0530

    Made cosmetic changes to the menu section

--- src/components/MenuSection.js
+++ src/components/MenuSection.js
@@ -1,16 +1,10 @@
 import React from "react";
-import AnimatedImage from "./AnimatedImage";
 
 export default function MenuSection({ category }) {
   return (
     <div className="card menu-section">
-      <AnimatedImage 
-        src={category.image} 
-        alt={category.displayName}
-        text={category.description}
-        className="menu-category-image"
-      />
       <h2>{category.displayName}</h2>
+      <p className="menu-description">{category.description}</p>
       <ul className="menu-list">
         {category.items.map((item, idx) => (
           <li key={idx}>
